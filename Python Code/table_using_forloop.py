20a73933da794307e53847d8798d282bdf435a40
Ashif Ali <aliashif569801@gmail.com>
2026-02-11 21:36:10 +0530

    this is my third commit

---
+++ Python Code/table_using_forloop.py
@@ -0,0 +1,31 @@
+# print("table of 17")
+while(True):
+    a = int(input("enter the searching of table number: "))
+    for i in range(1,11):
+        print(f"{a} X {i} = {a*i}")
+
+
+# print("\n\ntable of 18")
+# i = 18
+# while(i<181):
+#     print(i)
+
+#     i += 18
+
+# i = ["ashif"]
+# for name in i:
+#     print(name)
+# else:
+#     print("your program is done ")
+# print("\nchake the break function how can perform")
+# for i in range(6):
+#     if(i==3):
+#         break
+#     print(i)   
+
+# print("\nchake the continue function how can perform")
+
+# for i in range(6):
+#     if(i==3):
+#         continue
+#     print(i)    

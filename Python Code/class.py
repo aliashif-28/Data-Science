31a9f165856c01e160d027abefb7db6da6b8573a
Ashif Ali <aliashif569801@gmail.com>
2026-02-25 23:33:01 +0530

    my commit

---
+++ Python Code/class.py
@@ -0,0 +1,8 @@
+while(True):
+    class ashif:
+        name = input("enter your name: ")
+        sub = input("enter your sub: ")
+        def getInfo(self):
+            print(f"mr {self.name} and your subject is {self.sub}")
+    ali = ashif()
+    ali.getInfo()
